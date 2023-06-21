4d2e3da0c0dbe41876a552a2bb887fa50e8c40e0
Neto test <bolsvet190679@mail.ru>
2023-06-21 19:36:33 +0300

    modified

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         BmiService service = new BmiService();
         int weightInKg = 60;
         double heightInMiters = 1.62;
-        int bmi = service.calculate (weightInKg, heightInMiters);
+        int bmi = service.calculate(weightInKg, heightInMiters);
 
         System.out.println(bmi);
     }
